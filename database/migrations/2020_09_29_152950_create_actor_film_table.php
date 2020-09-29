2909ffc73cff76dfde0c4a6594690ee39cfd120f
k01yasik <bzdykin@mail.ru>
2020-09-29 18:33:23 +0300

    Adding models, controllers and migrations

---
+++ database/migrations/2020_09_29_152950_create_actor_film_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActorFilmTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actor_film', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actor_film');
+    }
+}
